3937b217e98d4e67b0ce4a9f61683f58e5dd33c1
ando07 <ando.masahito@gmail.com>
2019-05-28 08:22:46 +0900

    [add]removeEvent

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="hello">
-    <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions" v-on:vdropzone-sending="sendingEvent"></vue-dropzone>
+    <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions" v-on:vdropzone-sending="sendingEvent" v-on:vdropzone-removed-file="removeEvent"></vue-dropzone>
   </div>
 </template>
 
 <script>
   import vue2Dropzone from 'vue2-dropzone'
   import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+  import axios from 'axios'
 
   export default {
     name: 'HelloWsorld',
@@ -26,6 +27,13 @@
       // Add uuid to file for form data.
       sendingEvent: function(file, xhr, formData) {
         formData.append('uuid', file,upload.uuid)
+      },
+      removeEvent: function(file, error, xhr) {
+        axios.delete(`http://localhost:8888/images/$(file.upload.uuid}`).then(res => {
+          console.log(res.data)
+        }).catch(err => {
+          console.error(err)
+        })
       }
     }
   }
